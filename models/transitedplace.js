4796cec37de0fadcce7dfe31225d59fa9b58fa31
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-10-15 17:13:03 -0600

    add transited

--- models/transitedplace.js
+++ models/transitedplace.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     enable: DataTypes.BOOLEAN
   }, {
     sequelize,
+    schema: "business",
     modelName: 'TransitedPlace',
   });
   return TransitedPlace;
