8fc1dce54e6342e10b1e012f9fb904a95f7a1ce0
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-12 17:05:20 -0600

    add models

---
+++ migrations/20201112230107-create-alert.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Alerts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type:   Sequelize.ENUM,
+        values: ['En Tiempo', 'Verificar', 'Por llegar Tarde', 'Tarde']
+      },
+      time: {
+        type: Sequelize.DECIMAL
+      },
+      CompanyId: {
+        type: Sequelize.UUID,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        references: {
+          model: {
+            tableName: 'Companies',
+            schema: 'managers'
+          },
+          key: 'id'
+        },
+        comment: 'Reference to country',
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Alerts');
+  }
+};
