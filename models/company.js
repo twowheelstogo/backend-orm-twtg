3cc28d1306530108e1d157cbd17d5abf0512cf39
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2020-11-13 14:07:14 -0600

    add associations

--- models/company.js
+++ models/company.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
   class Company extends Model {
     static associate(models) {
       Company.hasMany(models.BranchOffice);
-      Company.belongsToMany(models.Alert, { through: models.CompanyAlert});
-      Company.belongsToMany(models.State, { through: models.CompanyState});
+      Company.hasMany(models.Alert);
+      Company.hasMany(models.Circle);
     }
   };
   Company.init({
